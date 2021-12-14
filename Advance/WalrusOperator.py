b0107a4584f04e50a413c381454beca4511e7522
Nishanth <innocentnishanth@gmail.com>
2021-12-14 21:29:12 +0530

    Added the Docs

---
+++ Advance/WalrusOperator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 11:39:26 2021
+
+@author: Nishanth
+"""
+
+var = 9
+
+print("Warlus Operator" if ( enter := int(input("Enter the value : ")) == var) else "sum")
+
+
+print( "Even Number" if (ev_od := int(input("Enter the number : "))  == 2 or 4 ) \
+      else "Odd Number")
